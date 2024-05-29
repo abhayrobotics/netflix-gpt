30d6c16b779fd6cbcd5ff73e26e5f5ad5a615b89
Abhay kumar <abhayrobotics1908@gmail.com>
2024-05-29 23:58:46 +0530

    tailwind change

--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -2,9 +2,10 @@
 const TitleContainer = ({title,overview}) => {
   return (
     <div className="pt-96 w-screen aspect-video px-8 absolute bg-gradient-to-r from-black">
-        <h2 className="font-bold text-6xl duration-2000  my-2 text-white">{title}</h2>   
+        <h2 className="font-bold text-6xl duration-1000  my-2 text-white">{title}</h2>   
         <p className="w-2/5 text-white">{overview}</p>   
         <div className="my-2"> 
+        
             <button className="cursor-pointer px-10 py-1 mr-2 font-semibold bg-white rounded-md bg-opacity-60 hover:bg-opacity-70" >  Play </button>
             <button className="cursor-pointer px-10 py-1 mr-2 font-semibold text-white bg-slate-700 rounded-md bg-opacity-60 hover:bg-opacity-70">More Info</button>
         </div>
