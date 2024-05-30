07fb4e3c74c51f9e7e07d2fb08af8047d84cfd1f
Abhay kumar <abhayrobotics1908@gmail.com>
2024-05-30 23:46:00 +0530

    cjheck

--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -6,6 +6,7 @@ const TitleContainer = ({title,overview}) => {
         <p className="w-2/5 text-white">{overview}</p>   
         <div className="my-2"> 
         
+        
             <button className="cursor-pointer px-10 py-1 mr-2 font-semibold bg-white rounded-md bg-opacity-60 hover:bg-opacity-70" >  Play </button>
             <button className="cursor-pointer px-10 py-1 mr-2 font-semibold text-white bg-slate-700 rounded-md bg-opacity-60 hover:bg-opacity-70">More Info</button>
         </div>
