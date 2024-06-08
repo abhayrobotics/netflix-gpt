7184dd0d7f808a211a1f37d5a42e77d3671c2e60
Abhay kumar <abhayrobotics1908@gmail.com>
2024-06-08 11:36:17 +0530

    new build and uninstall openai

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { netflixbackground } from "../utils/constant";
 import { lang } from "../utils/langConstant";
 import { useRef } from "react";
-import  {openai}  from "../utils/openai";
+
 import { model } from "../utils/geminiai";
 
 const SearchBar = () => {
@@ -19,14 +19,9 @@ const SearchBar = () => {
     const prompt =
       "Acting as Movie Recommentation system, giveme 5 results based on query: " +
       searchText.current.value +
-      ". give the results in semicolon separated. Example results: sholay; udaan; loki; hungama; kites";
+      ". give the results in semicolon separated in one line. Example results: sholay; udaan; loki; hungama; kites";
     
-    //   const completion = await openai.chat.completions.create({
-    //     messages: [{ role: "system", content: GPTquery }],
-    //     model: "gpt-3.5-turbo",
-    //   });
-
-    //   console.log(completion.choices);
+   
 
     const result = await model.generateContent(prompt)
     const response = await result.response;
--- src/utils/openai.js
+++
@@ -1,7 +0,0 @@
-import OpenAI from "openai";
-import { openAiKey } from "../utils/constant";
-
-export const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_Open_AI,
-  
-});
